9ddff216e488fc0f3556ea62ba07d7df094b74ad
Fede02027 <federica.santisi@studio.unibo.it>
2025-05-09 11:28:47 +0200

    fix euclide

--- Euclide.m
+++ Euclide.m
@@ -12,112 +12,129 @@ Get["Buttons.m"];
 Get["Aiuto.m"];
 
 EuclideComponent[a_Integer, b_Integer, stepsToComplete_Integer, onSuccessCallback_Function] := 
-  DynamicModule[{
-    currentA = a, currentB = b, steps = {}, currentStep = 1,
-    userQuotient = "", userRemainder = "", errorMessage = "", 
-    isCompleted = False, nextA = "", nextB = "", userMCD = "", mcdMessage = ""
-  },
-    
-    Panel[
-      Column[{
-        Style["Algoritmo di Euclide", Bold, 14],
-        Dynamic[
-          Column[
-            Join[
-              Table[
-                With[{stepData = steps[[i]]},
-                  Row[{
-                    Style[Row[{"Passo ", i}], Bold],
-                    "     a = ", stepData[[1]], 
-                    " b = ", stepData[[2]], 
-                    "     ", stepData[[1]], " div ", stepData[[2]], 
-                    " = ", stepData[[3]], ", resto ", stepData[[4]]
-                  }]
-                ],
-                {i, Length[steps]}
-              ],
-              
-              If[currentB === 0,
-                  {
-                    Style["Algoritmo completato! Il MCD \[EGrave] " <> ToString[currentA], Bold, 14],
-                    Button["Avanza di " <> ToString[stepsToComplete] <> " caselle",
-                      onSuccessCallback[currentA];
-                    ]
-                  },
-                  If[isCompleted,
-                  {
-                    Style["Inserisci i nuovi valori per a e b per continuare:", Bold],
-                    Row[{
-                      "a = ", InputField[Dynamic[nextA], Number, FieldSize -> 5], "   ",
-                      "b = ", InputField[Dynamic[nextB], Number, FieldSize -> 5]
-                    }],
-                    Dynamic[Style[errorMessage, Red] ],
-                    Button["Prosegui",
-                      If[nextA === currentB && nextB === Mod[currentA, currentB],
-                        currentA = nextA;
-                        currentB = nextB;
-                        userQuotient = "";
-                        userRemainder = "";
-                        nextA = "";
-                        nextB = "";
-                        errorMessage = "";
-                        isCompleted = False;
-                      ,
-                        errorMessage = "Risposta errata! Riprova."
-                      ]
-                    ]
-                  },
-                (* isCompleted is False *)
-                {
-                  (* Visualizzazione del attuale passo dell'algoritmo di Euclide *)
-                  Row[{
-                    Style[Row[{"Passo ", currentStep}], Bold],
-                    "    a = ", currentA,
-                    ", b = ", currentB,
-                    "     ", currentA, " div ", currentB,
-                    " = ", InputField[Dynamic[userQuotient], Number, FieldSize -> 5],
-                    ", resto ",
-                    InputField[Dynamic[userRemainder], Number, FieldSize -> 5]
-                  }],
-                  
-                  Dynamic[Style[errorMessage, Red] ],
-                  
-                  ClearFields[userQuotient, userRemainder, errorMessage],
-                  
-                  Button["Aiuto",
-                    Aiuto`MostraAiuto[currentA, currentB]
-                  ],
-                  
-                  Button["Verifica",
-                    If[userQuotient === Quotient[currentA, currentB] && 
-                       userRemainder === Mod[currentA, currentB],
-                      AppendTo[steps, {currentA, currentB, userQuotient, userRemainder}];
-                      currentStep++;
-                      userQuotient = "";
-                      userRemainder = "";
-                      errorMessage = "";
-                      isCompleted = True;
-                    ,
-                      errorMessage = "Risposta errata! Riprova."
-                    ]
-                  ]
-                }
-              ]
-            ]
+ DynamicModule[{
+   currentA = a, currentB = b, steps = {}, currentStep = 1,
+   userQuotient = "", userRemainder = "", errorMessage = "", 
+   isCompleted = False, nextA = "", nextB = "", userMCD = "", mcdMessage = ""
+ },
+  
+  Panel[
+   Column[{
+     Style["Algoritmo di Euclide", Bold, 14],
+     Dynamic[
+      Column[
+       Join[
+        Table[
+         With[{stepData = steps[[i]]},
+          Row[{
+            Style[Row[{"Passo ", i}], Bold],
+            "     a = ", stepData[[1]], 
+            " b = ", stepData[[2]], 
+            "     ", stepData[[1]], " div ", stepData[[2]], 
+            " = ", stepData[[3]], ", resto ", stepData[[4]]
+          }]
+         ],
+         {i, Length[steps]}
+        ],
+        
+        If[currentB === 0,
+         {
+          Style["Algoritmo completato! Il MCD \[EGrave] " <> ToString[currentA], Bold, 14],
+          Button["Avanza di " <> ToString[stepsToComplete] <> " caselle",
+           onSuccessCallback[currentA]
           ]
+         },
+         
+         If[isCompleted,
+          {
+           Style["Inserisci i nuovi valori per a e b per continuare:", Bold],
+           Row[{
+             "a = ", InputField[Dynamic[nextA], Number, FieldSize -> 5],
+             "   ",
+             "b = ", InputField[Dynamic[nextB], Number, FieldSize -> 5]
+           }],
+           Dynamic[Style[errorMessage, Red]],
+           Row[{
+             Button["Prosegui",
+              If[nextA === currentB && nextB === Mod[currentA, currentB],
+               currentA = nextA;
+               currentB = nextB;
+               userQuotient = "";
+               userRemainder = "";
+               nextA = "";
+               nextB = "";
+               errorMessage = "";
+               isCompleted = False;
+               ,
+               errorMessage = "Risposta errata! Riprova."
+              ]
+             ],
+             Button["Pulisci campi",
+              (nextA = ""; nextB = ""; errorMessage = ""),
+              ImageMargins -> 5
+             ]
+           }]
+          },
+          
+          {
+           Row[{
+             Style[Row[{"Passo ", currentStep}], Bold],
+             "    a = ", currentA,
+             ", b = ", currentB,
+             "     ", currentA, " div ", currentB,
+             " = ", InputField[Dynamic[userQuotient], Number, FieldSize -> 5],
+             ", resto ",
+             InputField[Dynamic[userRemainder], Number, FieldSize -> 5]
+           }],
+           
+           Dynamic[Style[errorMessage, Red]],
+           
+           ClearFields[userQuotient, userRemainder, errorMessage],
+           
+           Button["Aiuto",
+            Aiuto`MostraAiuto[currentA, currentB]
+           ],
+           
+           Button["Verifica",
+            If[userQuotient === Quotient[currentA, currentB] && 
+               userRemainder === Mod[currentA, currentB],
+             AppendTo[steps, {currentA, currentB, userQuotient, userRemainder}];
+             currentStep++;
+             If[Mod[currentA, currentB] === 0,
+              (* Algoritmo completato *)
+              currentB = 0; (* Forza il completamento *)
+             ,
+              isCompleted = True;
+             ];
+             userQuotient = "";
+             userRemainder = "";
+             errorMessage = "";
+             ,
+             errorMessage = "Risposta errata! Riprova.";
+             userQuotient = ""; userRemainder = "";
+            ]
+           ]
+          }
+         ]
         ]
-        ]
-      }, Spacings -> 1],
-      ImageMargins -> 10
-    ]
-  ];
+       ]
+      ]
+     ]
+    }, Spacings -> 1],
+   ImageMargins -> 10
+  ]
+ ]
 
 EuclideDialog[a_Integer, b_Integer, stepsToComplete_Integer, onSuccessCallback_Function] := 
-  CreateDialog[
-    EuclideComponent[a, b, stepsToComplete, onSuccessCallback],
-    WindowTitle -> "Algoritmo di Euclide",
-    Modal -> True
-  ];
+ CreateDialog[
+  EuclideComponent[a, b, stepsToComplete, onSuccessCallback],
+  WindowTitle -> "Algoritmo di Euclide",
+  Modal -> True
+ ]
 
 End[]
 EndPackage[]
+
+
+
+

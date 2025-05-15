9055d2cd9f7bda85513d3bf38a36e28c64ea0eeb
Enrico <enricofen300@gmail.com>
2025-05-15 17:49:26 +0200

    fix: correct spacing in visual representation explanation

--- Aiuto.m
+++ Aiuto.m
@@ -88,8 +88,8 @@ MostraAiuto[a_Integer, b_Integer] := Module[
 	Row[{
     "Immagina di divere il numero totale di palline ", Style[a, Bold],
     "(pari al dividendo) nel maggiore numero di gruppo possibili di ", Style[b, Bold],
-    "palline (divisore). Il numero di gruppi formatosi di ", Style[b, Bold],
-    "palline \[EGrave] il risultato della divisione. Le palline rimanenti dalla divisione in gruppi uguali \[EGrave] il resto"
+    " palline (divisore). Il numero di gruppi formatosi di ", Style[b, Bold],
+    " palline \[EGrave] il risultato della divisione. Le palline rimanenti dalla divisione in gruppi uguali \[EGrave] il resto"
     }]
      (* Visualizzazione dei gruppi (il divisore ripetuto q volte) *)
      Column[gruppi],

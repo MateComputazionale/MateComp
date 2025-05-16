efdb0b3b12948190a256877811c5c850cedc3416
Enrico <enricofen300@gmail.com>
2025-05-16 09:18:15 +0200

    ciao

--- Aiuto.m
+++ Aiuto.m
@@ -85,12 +85,9 @@ MostraAiuto[a_Integer, b_Integer] := Module[
      }],
      
      (* Spiegazione testuale della divisione *)
-	Row[{
-    "Immagina di divere il numero totale di palline ", Style[a, Bold],
-    " (pari al dividendo) nel maggiore numero di gruppo possibili di ", Style[b, Bold],
-    " palline (divisore). Il numero di gruppi formatosi di ", Style[b, Bold],
-    " palline \[EGrave] il risultato della divisione. Le palline rimanenti dalla divisione in gruppi uguali \[EGrave] il resto"
-    }]
+
+    "Immagina di divere il numero totale di palline " <> ToString[a] <> " (pari al dividendo) nel maggiore numero di gruppo possibili di " <> ToString[b] <> " palline (divisore). Il numero di gruppi formatosi di " <> ToString[b] <> " palline \[EGrave] il risultato della divisione. Le palline rimanenti dalla divisione in gruppi uguali \[EGrave] il resto"
+
      (* Visualizzazione dei gruppi (il divisore ripetuto q volte) *)
      Column[gruppi],
      
